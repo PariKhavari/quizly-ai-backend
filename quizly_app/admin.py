5dd334ca0ab77c373df192536d96fdebf867db91
pari khavari <khavaripari60@gmail.com>
2026-01-19 11:53:01 +0100

    feat(admin): manage quizzes and questions via Django admin

--- quizly_app/admin.py
+++ quizly_app/admin.py
@@ -6,6 +6,9 @@ from .models import Quiz, Question
 class QuestionInline(admin.TabularInline):
     model = Question
     extra = 0
+    fields = ("question_title", "question_options", "answer", "created_at", "updated_at")
+    readonly_fields = ("created_at", "updated_at")
+    show_change_link = True
 
 
 @admin.register(Quiz)
@@ -13,6 +16,8 @@ class QuizAdmin(admin.ModelAdmin):
     list_display = ("id", "title", "user", "created_at", "updated_at")
     list_filter = ("created_at", "updated_at")
     search_fields = ("title", "description", "video_url", "user__username", "user__email")
+    readonly_fields = ("created_at", "updated_at")
+    fields = ("user", "title", "description", "video_url", "created_at", "updated_at")
     inlines = [QuestionInline]
 
 
@@ -21,3 +26,5 @@ class QuestionAdmin(admin.ModelAdmin):
     list_display = ("id", "quiz", "created_at", "updated_at")
     list_filter = ("created_at", "updated_at")
     search_fields = ("question_title", "answer", "quiz__title")
+    readonly_fields = ("created_at", "updated_at")
+    fields = ("quiz", "question_title", "question_options", "answer", "created_at", "updated_at")
